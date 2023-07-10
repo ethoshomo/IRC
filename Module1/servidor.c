ef93e3f76bc389e5ef16ab0780936e269c96e082
Carlos Filipe de Castro Lemos <ethoshomo@gmail.com>
2023-07-10 17:02:23 -0300

    Updates

--- Module1/servidor.c
+++ Module1/servidor.c
@@ -15,17 +15,15 @@
 #define FAIL -1
 #define ERROR 2
 
-int main()
-{
+int main(){
 
-    /*
-        Para que o servidor possa funcionar, é
-        necessário ter: a) dois sockets para
-        armazenar as informações de conexão do
-        cliente e do servidor; b) um buffer para
-        armazenar as informações de comunicação
-        (cujo tamanho é pre-estabelecido em 4096).
-    */
+    /////////////////////////////////////// SERVIDOR
+    //   Para que o servidor possa funcionar, é
+    //   necessário ter: a) dois sockets para
+    //   armazenar as informações de conexão do
+    //   cliente e do servidor; b) um buffer para
+    //   armazenar as informações de comunicação
+    //   (cujo tamanho é pre-estabelecido em 4096).
 
     // Criação do buffer de dados
     char buffer[BUFFER];
@@ -34,26 +32,26 @@ int main()
     int serverDecriptor, clientDescriptor;
     struct sockaddr_in server, client;
     
-    /*
-        O socket permite que processos diferentes
-        se comuniquem. Nesse caso, a criação do 
-        socket IPv4 envolve o uso da função socket(). 
-        
-        Essa função recebe como parâmetros informações 
-        para criar uma conexão TCP (STREAM_SOCKET, ou 
-        seja, criar circuitos virtuais) e usar o 
-        protocolo IPv4 (usando o parâmetro AF_INET). 
-        Como retorno, ela devolve um ponteiro para 
-        ser usado como descritor de arquivos.
-        
-        Caso o socket seja criado com sucesso, é
-        preciso configurar as informações na struct 
-        do servidor que foi criada acima. Assim, 
-        configuramos as chaves sin_family, sin_port 
-        e sin_zero.
-    */
 
-    //////////////////////////// SOCKETS
+    /////////////////////////////////////// SOCKETS
+    //
+    //    O socket permite que processos diferentes
+    //    se comuniquem. Nesse caso, a criação do 
+    //    socket IPv4 envolve o uso da função socket(). 
+    //    
+    //    Essa função recebe como parâmetros informações 
+    //    para criar uma conexão TCP (STREAM_SOCKET, ou 
+    //    seja, criar circuitos virtuais) e usar o 
+    //    protocolo IPv4 (usando o parâmetro AF_INET). 
+    //    Como retorno, ela devolve um ponteiro para 
+    //    ser usado como descritor de arquivos.
+    //    
+    //    Caso o socket seja criado com sucesso, é
+    //    preciso configurar as informações na struct 
+    //    do servidor que foi criada acima. Assim, 
+    //    configuramos as chaves sin_family, sin_port 
+    //    e sin_zero.
+
     // Criação do Socket. É passado o domínio
     // IPv4, o tipo de conexão (TCP - SOCK_STREAM)
     // e o protocolo (0).
@@ -65,12 +63,13 @@ int main()
     server.sin_port = htons(PORT);
     memset(server.sin_zero, 0x0, 8);
 
-    /*
-        Caso o socket tenha sido criado com sucesso, é preciso
-        realizar a vinculação entre o socket e a porta.    
-    */
+
 
     //////////////////////////// BIND
+    //   Caso o socket tenha sido criado com sucesso, é preciso
+    //   realizar a vinculação entre o socket e a porta.    
+    
+
     // Verifica se a porta está em uso
     int check = 1;
     int returnSetSocket = setsockopt(serverDecriptor, SOL_SOCKET, SO_REUSEADDR, &check, sizeof(int));
@@ -86,13 +85,11 @@ int main()
         return ERROR;
     }
 
-    /*
-        Caso tudo tenha ocorrido sem erros, o programa do servidor
-        irá executar a função listen() e irá bloquear o processo
-        até que um cliente tente fazer conexão.
-    */
-
     //////////////////////////// LISTEN
+    //    Caso tudo tenha ocorrido sem erros, o programa do servidor
+    //    irá executar a função listen() e irá bloquear o processo
+    //    até que um cliente tente fazer conexão.
+    
     // O programa ficará ouvindo a porta. Caso haja conexão,
     // a comunicação se inicia.
     int returnListen = listen(serverDecriptor, 1);
@@ -101,13 +98,12 @@ int main()
         return ERROR;
     }
 
-    /*
-        O processo permanecerá bloqueado até que o cliente faça
-        a conexão pelo método do handshake, o que é implementado
-        pela função accept().
-    */
-
+    
     //////////////////////////// ACCEPT
+    //    O processo permanecerá bloqueado até que o cliente faça
+    //    a conexão pelo método do handshake, o que é implementado
+    //    pela função accept().
+    
     socklen_t client_len = sizeof(client);
     clientDescriptor = accept(serverDecriptor, (struct sockaddr *)&client, &client_len);
     if (clientDescriptor == FAIL){
@@ -115,27 +111,27 @@ int main()
         return ERROR;
     }
 
-    /*
-        Caso a conexão seja estabelecida entre cliente e
-        servidor, podemos iniciar a comunicação através
-        de uma mensagem de boas vindas ao servidor.
 
-        O modus operandi da comunicação consiste em o
-        servidor permanecer disponível para receber mensagens
-        do cliente, responder a mensagem recebida e ficar
-        disponível para receber outra mensagem. Esse processo
-        acontece indefinidamente, desde que não receba a
-        mensagem "/quit".
-
-        Caso receba a mensagem "/quit", a conexão será
-        desfeita e o servidor será encerrado.
-    */
+    //////////////////////////// COMUNICAÇÃO
+    //    Caso a conexão seja estabelecida entre cliente e
+    //    servidor, podemos iniciar a comunicação através
+    //    de uma mensagem de boas vindas ao servidor.
+    //
+    //    O modus operandi da comunicação consiste em o
+    //    servidor permanecer disponível para receber mensagens
+    //    do cliente, responder a mensagem recebida e ficar
+    //    disponível para receber outra mensagem. Esse processo
+    //    acontece indefinidamente, desde que não receba a
+    //    mensagem "/quit".
+    //
+    //    Caso receba a mensagem "/quit", a conexão será
+    //    desfeita e o servidor será encerrado.
+    
 
     strcpy(buffer, "Seja bem vindo!\n\0");
 
     int returnSend = send(clientDescriptor, buffer, strlen(buffer), 0);
-    if (returnSend)
-    {
+    if (returnSend){
         fprintf(stdout, "Cliente conectado com sucesso.\n");
         fprintf(stdout, "Aguardando mensagens!\n");
 
